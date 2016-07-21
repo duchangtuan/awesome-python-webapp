326c59a365fd25525a84634b3e653c6848f85989
fengxi-ubuntu <fengxi1986@gmail.com>
2016-07-21 23:12:31 +0800

    Complete 99% of the orm.py

--- www/transwarp/orm.py
+++ www/transwarp/orm.py
@@ -10,29 +10,29 @@ import time, logging, db
 class Field(object):
      _count = 0
 
-    def __init__(self, **kw):
-        self.name = kw.get('name', None)
-        self._default = kw.get('default', None)
-        self.primary_key = kw.get('primary_key', False)
-        self.nullable = kw.get('nullable', False)
-        self.updatable = kw.get('updatable', True)
-        self.insertable = kw.get('insertable', True)
-        self.ddl = kw.get('ddl', '')
-        self._order = field._count
-        Field._count = Field._count + 1
+     def __init__(self, **kw):
+         self.name = kw.get('name', None)
+         self._default = kw.get('default', None)
+         self.primary_key = kw.get('primary_key', False)
+         self.nullable = kw.get('nullable', False)
+         self.updatable = kw.get('updatable', True)
+         self.insertable = kw.get('insertable', True)
+         self.ddl = kw.get('ddl', '')
+         self._order = Field._count
+         Field._count = Field._count + 1
 
-    @property
-    def default(self):
-        d = self._default
-        return d() if callable(d) else d
+     @property
+     def default(self):
+         d = self._default
+         return d() if callable(d) else d
 
-    def __str__(self):
-        s = ['<%s:%s,%s,default(%s),>' %(self.__class__.__name__, self.name, self.ddl, self._default)] 
-        self.nullable and s.append('N')
-        self.updateable and s.append('U')
-        self.insertable and s.append('I')
-        s.append('>')
-        return ''.join(s)
+     def __str__(self):
+         s = ['<%s:%s,%s,default(%s),>' %(self.__class__.__name__, self.name, self.ddl, self._default)] 
+         self.nullable and s.append('N')
+         self.updatable and s.append('U')
+         self.insertable and s.append('I')
+         s.append('>')
+         return ''.join(s)
 
 class StringField(Field):
     def __init__(self, **kw):
@@ -154,3 +154,155 @@ class ModelMetaclass(type):
             if not trigger in attrs:
                 attrs[trigger] = None
         return type.__new__(cls, name, bases, attrs)
+
+class Model(dict):
+    '''
+    Base class for ORM.
+
+    >>> class User(Model):
+    ...     id = IntegerField(primary_key=True)
+    ...     name = StringField()
+    ...     email = StringField(updatable=False)
+    ...     passwd = StringField(default=lambda: '******')
+    ...     last_modified = FloatField()
+    ...     def pre_insert(self):
+    ...         self.last_modified = time.time()
+    >>> u = User(id=10190, name='Michael', email='orm@db.org')
+    >>> r = u.insert()
+    >>> u.email
+    'orm@db.org'
+    >>> u.passwd
+    '******'
+    >>> u.last_modified > (time.time() - 2)
+    True
+    >>> f = User.get(10190)
+    >>> f.name
+    u'Michael'
+    >>> f.email
+    u'orm@db.org'
+    >>> f.email = 'changed@db.org'
+    >>> r = f.update() # change email but email is non-updatable
+    >>> len(User.find_all())
+    1
+    >>> g = User.get(10190)
+    >>> g.email
+    u'orm@db.org'
+    >>> r = g.delete()
+    >>> len(db.select('select * from user where id=10190'))
+    0
+    >>> import json
+    >>> print User().__sql__()
+    -- generating SQL for user:
+    create table `user` (
+      `id` bigint not null,
+      `name` varchar(255) not null,
+      `email` varchar(255) not null,
+      `passwd` varchar(255) not null,
+      `last_modified` real not null,
+      primary key(`id`)
+    );
+    '''
+    __metaclass__ = ModelMetaclass
+    
+    def __init__(self, **kw):
+        super(Model, self).__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    @classmethod
+    def get(cls, pk):
+        '''
+        Get by primary key.
+        '''
+        d = db.select_one('select * from %s where %s=?' %(cls.__table__, cls.__primary_key__.name), pk)
+        return cls(**d) if d else None
+
+    @classmethod
+    def find_first(cls, where, *args):
+        '''
+        Find by where clause and return one result. If multiple results found.
+        only the first one returned. If no result found, return None.
+        '''
+        d = db.select_one('select * from %s %s' %(cls.__table__, where), *args)
+        return cls(**d) if d else None
+
+    @classmethod
+    def find_all(cls, *args):
+        '''
+        Find all and return list
+        '''
+        L = db.select('select * from `%s`' % cls.__table__)
+        return [cls(**d) for d in L]
+
+    @classmethod
+    def find_by(cls, where, *args):
+        '''
+        Find by where clause and return list.
+        '''
+        L = db.select('select * from `%s` %s' %(cls.__table__, where), *args)
+        return [cls(**d) for d in L]
+
+    @classmethod
+    def count_all(cls):
+        '''
+        Find by 'select count(pk) from table' and return integer.
+        '''
+        return db.select_int('select count(`%s`) from `%s`' % (cls.__primary_key__.name, cls.__table__))
+
+    @classmethod
+    def count_by(cls, where, *args):
+        '''
+        Find by 'select count(pk) from table where ...' and return int.
+        '''
+        return db.select_int('select count(`%s`) from `%s` %s' % (cls.__primary_key__.name, cls.__table__, where), *args)
+
+    def update(self):
+        self.pre_update and self.pre_update()
+        L = []
+        args = []
+        for k, v in self.__mappings__.iteritems():
+            if v.updatable:
+                if hasattr(self, k):
+                    arg = getattr(self, k)
+                else:
+                    arg = v.default
+                    setattr(self, k, arg)
+                L.append('`%s`=?' %k)
+                args.append(arg)
+        pk = self.__primary_key__.name
+        args.append(getattr(self, pk))
+        db.update('update `%s` set %s where %s=?' % (self.__table__, ','.join(L), pk), *args)
+        return self
+
+    def delete(self):
+        self.pre_delete and self.pre_delete()
+        pk = self.__primary_key__.name
+        args = (getattr(self, pk), )
+        db.update('delete from `%s` where `%s`=?' % (self.__table__, pk), *args)
+        return self
+    
+    def insert(self):
+        self.pre_insert and self.pre_insert()
+        params = {}
+        for k, v in self.__mappings__.iteritems():
+            if v.insertable:
+                if not hasattr(self, k):
+                    setattr(self, k, v.default)
+                params[v.name] = getattr(self, k)
+        db.insert('%s' % self.__table__, **params)
+        return self
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    db.create_engine('root', 'root', 'test')
+    db.update('drop table if exists user')
+    db.update('create table user (id int primary key, name text, email text, passwd text, last_modified real)')
+    import doctest
+    doctest.testmod()
