cac432a71d4b28354acd926a9034ca753d06a26d
fengxi-ubuntu <fengxi1986@gmail.com>
2016-07-28 22:19:16 +0800

    The thrid day to write web.py

--- www/transwarp/web.py
+++ www/transwarp/web.py
@@ -364,3 +364,213 @@ def redirect(location):
     RedirectError: 301 Moved Permenently, http://www.itranswarp.com/
     '''
     return RedirectError(301, location)
+
+def fount(location):
+    '''
+    Do temporary redirect.
+
+    >>> raise found('http://www.itranswarp.com/')
+    Traceback (most recent call last):
+      ...
+    RedirectError: 302 Found, http://www.itranswarp.com/
+    '''
+    return RedirectError(302, location)
+
+def seeother(location):
+    '''
+    Do temporary redirect.
+
+    >>> raise seeother('http://www.itranswarp.com/')
+    Traceback (most recent call last):
+      ...
+    >>> e = seeother('http://ww.itranswarp.com/seeother?r=123')
+    >>> e.location
+    'http://www.itranswarp.com/seeother?r=123'
+    '''
+    return RedirectError(303, location)
+
+def _to_str(s):
+    '''
+    Convert to str.
+
+    >>> _to_str('s123') == 's123'
+    True
+    >>> _to_str(u'\u4e2d\u6587') == '\xe4\xb8\xad\xe6\x96\x87'
+    True
+    >>> _to_str(-123) == '-123'
+    True
+    '''
+    if isinstance(s, str):
+        return s
+    if isinstance(s, unicode):
+        return s.encode('utf-8')
+    return str(s)
+
+def _to_unicode(s):
+    '''
+    Convert to unicode.
+
+    >>> _to_unicode('\xe4\xb8\xad\xe6\x96\x87') == u'\u4e2d\u6587'
+    True
+    '''
+    return s.decode('utf-8')
+
+def _quote(s, encoding='utf-8'):
+    '''
+    Url quote as str.
+
+    >>> _quote('http://example/test?a=1+')
+    'http%3A//example/test%3Fa%3D1%2B'
+    >>> _quote(u'hello world!')
+    'hello%20world%21'
+    '''
+    if isinstance(s, unicode):
+        s = s.encode(encoding)
+    return urllib.quote(s)
+
+def _unquote(s, encoding='utf-8'):
+    '''
+    Url unquote as unicode.
+
+    >>> _unquote('http%3A//example/test%3Fa%3D1+')
+    u'http://example/test?a=1+'
+    '''
+    return urllib.unquote(s).decode(encoding)
+
+def get(path):
+    '''
+    A @get decoder.
+
+    @get('/:id')
+    def index(id):
+        pass
+
+    >>> @get('/test/:id')
+    ... def test():
+    ...     return 'ok'
+    ...
+    >>> test.__web_route__
+    '/test/:id'
+    >>> test.__web_method__
+    'GET'
+    >>> test()
+    'ok'
+    '''
+    def _decorator(func):
+        func.__web_route__ = path
+        func.__web_method__ = 'GET'
+        return func
+    return _decorator
+
+def post(path):
+    '''
+    A @post decorator.
+
+    >>> @post('/post/:id')
+    ... def testpost():
+    ...     return '200'
+    ...
+    >>> testpost.__web_route__
+    '/post/:id'
+    >>> testpost.__web_method__
+    'POST'
+    >>> testpost()
+    '200'
+    '''
+    def _decorator(func):
+        func.__web_route__ = path
+        func.__web_method__ = 'POST'
+        return func
+    return _decorator
+
+_re_route = re.compile(r'(\:[a-zA-Z_]\w)')
+
+def _build_regex(path):
+    r'''
+    Convert route path to regex.
+
+    >>> _build_regex('/path/to/:file')
+    '^\\/path\\to\\/(?P<file>[^\\/]+)$'
+    >>> 
+    '''
+    re_list = ['^']
+    var_list = []
+    is_var = False
+    for v in _re_route.split(path):
+        if is_var:
+            var_name = v[1:]
+            var_list.append(var_name)
+            re_list.append(r'(?P<%s>[^\/]+)' % var_name)
+        else:
+            s = ''
+            for ch in v:
+                if ch>='0' and ch<='9':
+                    s = s + ch
+                elif ch>='A' and ch<='Z':
+                    s = s + ch
+                elif ch>='a' and ch<='z':
+                    s = s + ch
+                else:
+                    s = s + '\\' + ch
+            re_list.append(s)
+        is_var = not is_var
+    re_list.append('$') 
+    return ''.join(re_list)
+
+class Route(object):
+    '''
+    A Route object is a callable object.
+    '''
+
+    def __init__(self, func):
+        self.path = func.__web_route__
+        self.method = func.__web_method__
+        self.is_static = _re_route.search(self.path) is None
+        if not self.is_static:
+            self.route = re.compile(_build_regex(self.path))
+        self.func = func
+
+    def match(self, url):
+        m = self.route.match(url)
+        if m:
+            return m.groups()
+        return None
+
+    def __call__(self, *args):
+        return self.func(*args)
+
+    def __str__(self):
+        if self.is_static:
+            return 'Route(static, %s, path=%s)' % (self.method, self.path)
+        return 'Route(dynamic, %s, path=%s)' % (self.method, self.path)
+
+    __repr__ = __str__
+
+def _static_file_generator(fpath):
+    BLOCK_SIZE = 8192
+    with open(fpath, 'rb') as f:
+        block = f.read(BLOCK_SIZE)
+        while block:
+            yield block
+            block = r.read(BLOCK_SIZE)
+
+class StaticFileRoute(object):
+    def __init__(self):
+        self.method = 'GET'
+        self.is_static = False
+        self.route = re.compile('^/static/(.+)$')
+
+    def match(self, url):
+        if usl.startwith('/static/'):
+            return (url[1:], )
+        return None
+
+    def __call__(self, *args):
+        fpath = os.path.join(ctx.application.document_root, args[0])
+        if not os.path.isfile(fpath):
+            raise notfound()
+        ftext = os.path.splitext(fpath)[1]
+        ctx.response.content_type = mimetypes.types_map.get(fext.lower(), 'application/octest-stream')
+        return _static_file_generator(fpath)
+
+
