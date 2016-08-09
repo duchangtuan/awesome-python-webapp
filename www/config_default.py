4be5d628031381bf257a5b516b62d9cea40def6a
fengxi-ubuntu <fengxi1986@gmail.com>
2016-08-09 21:56:39 +0800

    Add config files

---
+++ www/config_default.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Default configuration.
+'''
+
+configs = {
+    'db':{
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': 'root',
+        'database': 'awesome'
+    },
+    'session': {
+        'secret':'AwEsOmE'
+    }
+}
