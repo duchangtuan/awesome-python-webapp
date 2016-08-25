7f838083ef04f274c56c288e4568c72f5d672922
fengxi-ubuntu <fengxi1986@gmail.com>
2016-08-25 23:09:44 +0800

    Add RESTful api

--- www/urls.py
+++ www/urls.py
@@ -1,5 +1,6 @@
 #urls.py
 from transwarp.web import get, view
+from apis import api, APIError, APIValueError, APIPermissionError, APIResourceNotFoundError
 from models import User, Blog, Comment
 
 @view('blogs.html')
@@ -9,3 +10,11 @@ def test_users():
     # find the login users
     user = User.find_first('where email=?', 'fengxi1986@gmail.com')
     return dict(blogs=blogs, user=user)
+
+@api
+@get('/api/users')
+def api_get_users():
+    users = User.find_by('order by created_at desc')
+    for u in users:
+        u.password = '******'
+    return dict(users=users)
